d44d03f575d91d26579e21e0c0e010b88526ebfc
Gilberto Ficara <g.ficara@oltrelinux.com>
2010-03-05 18:20:23 +0100

    Logging OK

--- catstatus.rb
+++ catstatus.rb
@@ -5,7 +5,7 @@ require 'haml'
 set :haml, {:format => :html5 } # default Haml format is :xhtml
 
 statuses = {
-    0 => 'Non raggiungibile, non pervenuto',
+    0 => 'Non è in zona',
     1 => 'Dorme',
     2 => 'Sonnecchia e/o si fa fare le coccole',
     3 => 'Guarda fuori dalla finestra',
@@ -14,17 +14,23 @@ statuses = {
     6 => 'Gioca moderatamente',
     7 => 'Gioca rumorosamente',
     8 => 'Si comporta da pazzo',
-    9 => 'Posseduto da forze demoniache'
+    9 => 'E` Posseduto da forze demoniache'
 }
 
 get '/' do
+  content_type 'text/html', :charset => 'utf-8'
   @statuses = statuses
   haml :index
 end
 
 get '/status/add/:status' do
   @statuses = statuses
-  puts "[#{Time.now}] Called with status: #{params[:status]}\n"
+  puts "
+[#{Time.now}] 
+il gatto #{@statuses[params[:status].to_i]}
+(valore: #{params[:status]})
+
+"
   redirect '/'
 end
 
