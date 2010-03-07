987d78ce6bb1fc4a6872f4f550f5f03603dde806
Gilberto Ficara <velenux@paguro.(none)>
2010-03-07 01:06:19 +0100

    Aggiunti CSS, ancora zero implementazione dell'azione su click...

--- catstatus.rb
+++ catstatus.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
 
-set :haml, {:format => :html5 } # default Haml format is :xhtml
-
+version  = '0.0.1'
 statuses = {
     0 => 'Non è in zona',
     1 => 'Dorme',
@@ -11,16 +11,23 @@ statuses = {
     3 => 'Guarda fuori dalla finestra',
     4 => 'Fissa un punto sopra la vostra testa',
     5 => 'Si aggira per casa',
-    6 => 'Gioca moderatamente',
-    7 => 'Gioca rumorosamente',
-    8 => 'Si comporta da pazzo',
-    9 => 'E` Posseduto da forze demoniache'
+    6 => 'Mangia',
+    7 => 'Gioca moderatamente',
+    8 => 'Gioca rumorosamente',
+    9 => 'Si comporta da pazzo',
+  10 => 'E` Posseduto da forze demoniache'
 }
 
 get '/' do
-  content_type 'text/html', :charset => 'utf-8'
+  #content_type 'text/html', :charset => 'utf-8'
   @statuses = statuses
+  @version  = version
+  set :haml, {:format => :html5, :lang => 'it', :charset => 'utf-8' } # default Haml format is :xhtml
   haml :index
+end
+
+get '/style.css' do
+  sass :style
 end
 
 get '/status/add/:status' do
